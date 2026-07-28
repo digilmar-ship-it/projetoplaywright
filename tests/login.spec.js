f134eab3671fe1ed80dcec9600328f4498d52155
Gilmar Figueiredo da Silva <digilmar@gmail.com>
2026-07-28 16:07:37 -0300

    Criando page objects

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 const baseUrl = 'https://automationpratice.com.br';
 
@@ -16,26 +17,36 @@ test('Login com sucesso', async ({ page }) => {
 
 test('Login com email inválido', async ({ page }) => {
   await page.goto(baseUrl)
+  // Acessar página principal
   await page.getByRole('link', { name: ' Login' }).click();
+  //Acessar botão de login
   await page.locator('#user').fill('usuario@');
   await page.locator('#password').fill('senhaerrada');
+  //Preencher usuário e senha
   await page.getByRole('link', { name: 'Login' }).click();
+  //Clicar botão login
   await expect(page.getByText('E-mail inválido.')).toBeVisible
+  //Validar mensagem de E-mail inválido
 });
 
 test('Login com senha inválida', async ({ page }) => {
   await page.goto(baseUrl)
+  // Acessar página principal
   await page.getByRole('link', { name: ' Login' }).click()
+  //Acessar botão de login
   await page.locator('#user').fill('usuario@gmail.com')
   await page.locator('#password').fill('123')
+  //Preencher usuário e senha
   await page.getByRole('button', { name: 'login' }).click()
+  //Clicar botão login
   await expect(page.getByText('Senha inválida.')).toBeVisible
+  //Validar mensagem de senha inválida
 });
 
 test('Login sem dados', async ({ page }) => {
   await page.goto(baseUrl)
   await page.getByRole('link', { name: ' Login' }).click()
-    await page.getByRole('button', { name: 'login' }).click()
+  await page.getByRole('button', { name: 'login' }).click()
   await expect(page.getByText('E-mail inválido.')).toBeVisible
-  
+
 });
